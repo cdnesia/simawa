8dd326c7cb2826497f40a19caefd199423a20d79
mjumain93 <mjumain93@gmail.com>
2026-09-07 11:19:33 +0700

    update boleh kontrak

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -139,6 +139,8 @@ class PaymentService
 
         $response = $this->apiService->post('api/v1/tagihan/cek', $body);
 
+        dd($response);
+
         if ($response['error_code'] !== 0) {
             return false;
         }
